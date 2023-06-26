e761a4c5529df2a4e7757cd735f08c34babbe5dc
Kevyn Lopes <kevynlopes2019@gmail.com>
2023-06-26 17:37:48 -0300

    New projects

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ import Footer from "./components/Footer.vue";
     <Navbar />
     <Hero />
     <Projetos />
-    <Projetos />
     <Footer />
   </div>
 </template>
--- src/components/Projetos.vue
+++ src/components/Projetos.vue
@@ -1,47 +1,46 @@
 <template>
-  <div
-    class="flex flex-col items-center gap-8 pb-72 w-screen overflow-x-auto"
-    id="projetos"
-  >
-    <h1 class="text-6xl text-white p-4">Projetos</h1>
-    <div class="flex gap-10">
-      <div class="bg-white w-96 p-2 rounded-lg text-center">
-        <h2 class="p-2">Projeto Imobiliaria</h2>
-        <img src="../assets/projetos/imob.png" alt="projeto 1" />
-        <p class="p-2">
-          Projeto para uma Imobiliaria, com banco de dados e sistema de adição e
-          remoção de casas
-        </p>
-        <div>
-          <p class="font-bold">Ferramentas</p>
-          <ul>
-            <li>React</li>
-          </ul>
+  <div>
+    <div class="flex justify-center">
+      <h1 class="text-6xl text-white p-4">Projetos</h1>
+    </div>
+
+    <div
+      class="flex flex-col items-center gap-8 pb-72 w-screen overflow-x-auto"
+      id="projetos"
+    >
+      <div class="flex gap-10">
+        <div class="bg-white w-96 p-2 rounded-lg text-center">
+          <h2 class="p-2 font-semibold">Projeto Imobiliaria</h2>
+          <img src="../assets/projetos/imob.png" alt="projeto 1" />
+          <p class="p-2">Projeto para uma Imobiliaria.</p>
+          <div class="flex flex-col gap-2">
+            <p class="font-bold">Ferramentas</p>
+            <div class="flex justify-center">
+              <img src="../assets/react.png" alt="vue logo" width="20" />
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="bg-white w-96 p-2 rounded-lg text-center">
-        <h2 class="p-2">Projeto TodoList</h2>
-        <img src="../assets/projetos/todo.png" alt="projeto 1" />
-        <p class="p-2">Projeto Lista de tarefas simples.</p>
-        <div>
-          <p class="font-bold">Ferramentas</p>
-          <ul>
-            <li>Vue js</li>
-          </ul>
+        <div class="bg-white w-96 p-2 rounded-lg text-center">
+          <h2 class="p-2 font-semibold">Projeto TodoList</h2>
+          <img src="../assets/projetos/todo.png" alt="projeto 2" />
+          <p class="p-2">Projeto Lista de tarefas simples.</p>
+          <div class="flex flex-col gap-2">
+            <p class="font-bold">Ferramentas</p>
+            <div class="flex justify-center">
+              <img src="../assets/vue.png" alt="vue logo" width="20" />
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="bg-white w-96 p-2 rounded-lg text-center">
-        <h2 class="p-2">Projeto Techno</h2>
-        <img src="../assets/projetos/techno.png" alt="projeto 1" />
-        <p class="p-2">
-          Projeto para uma Imobiliaria, com banco de dados e sistema de adição e
-          remoção de casas
-        </p>
-        <div>
-          <p class="font-bold">Ferramentas</p>
-          <ul>
-            <li>Vue</li>
-          </ul>
+        <div class="bg-white w-96 p-2 rounded-lg text-center">
+          <h2 class="p-2 font-semibold">Projeto Techno</h2>
+          <img src="../assets/projetos/techno.png" alt="projeto 3" />
+          <p class="p-2">Projeto Lista de tarefas simples.</p>
+          <div class="flex flex-col gap-2">
+            <p class="font-bold">Ferramentas</p>
+            <div class="flex justify-center">
+              <img src="../assets/vue.png" alt="vue logo" width="20" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
